d52d461a7446bc0069edc2b2dc9093b826b8d51a
Georgios Adamidis <g.adamidis1985@gmail.com>
2023-10-17 22:20:55 +0300

    Users CRUD Scaffolding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,22 @@ import { PersonCardComponent } from './person-card/person-card.component';
 import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 import { AppService } from './app.service';
+import { CrudDemoComponent } from './crud-demo/crud-demo/crud-demo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, PersonComponent, PersonAltComponent, EventBindComponent, OutputDemoComponent, PersonCardComponent, TemplateDrivenFormComponent, ReactiveFormComponent],
+  imports: [
+    CommonModule, 
+    PersonComponent, 
+    PersonAltComponent, 
+    EventBindComponent, 
+    OutputDemoComponent, 
+    PersonCardComponent, 
+    TemplateDrivenFormComponent, 
+    ReactiveFormComponent, 
+    CrudDemoComponent
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
