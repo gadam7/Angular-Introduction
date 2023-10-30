f4b10c7feb50a49483d4dd8c5f959d3ac4982675
Georgios Adamidis <g.adamidis1985@gmail.com>
2023-10-30 20:59:12 +0200

    angular material table

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from "@angular/router";
 import { WelcomeComponent } from "./welcome/welcome.component";
 import { EventBindComponent } from "./event-bind/event-bind.component";
+import { CreateUserComponent } from "./crud-demo/crud/create-user/create-user.component";
+import { ReadUserComponent } from "./crud-demo/crud/read-user/read-user.component";
+import { UpdateUserComponent } from "./crud-demo/crud/update-user/update-user.component";
+import { DeleteUserComponent } from "./crud-demo/crud/delete-user/delete-user.component";
+import { ListUsersComponent } from "./crud-demo/utils/list-users/list-users.component";
 
 
 export const routes: Routes = [
     {path: 'home', component: WelcomeComponent},
-    {path: 'event-bind', component: EventBindComponent}
+    {path: 'event-bind', component: EventBindComponent},
+    {path: 'crud-demo/create', component: CreateUserComponent},
+    {path: 'crud-demo/read', component: ReadUserComponent},
+    {path: 'crud-demo/update', component: UpdateUserComponent},
+    {path: 'crud-demo/delete', component: DeleteUserComponent},
+    {path: 'crud-demo/list', component: ListUsersComponent},
 ];
--- src/app/crud-demo/utils/list-users/list-users.component.ts
+++ src/app/crud-demo/utils/list-users/list-users.component.ts
@@ -1,22 +1,77 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  Inject,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppService } from 'src/app/app.service';
 import { Person } from 'src/app/interfaces/person';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatCardModule } from '@angular/material/card';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-list-users',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule, MatSortModule, MatCardModule],
   templateUrl: './list-users.component.html',
-  styleUrls: ['./list-users.component.css']
+  styleUrls: ['./list-users.component.css'],
 })
 export class ListUsersComponent implements OnInit {
+  @ViewChild(MatSort) sort!: MatSort;
+
   users: Person[] = [];
-  constructor(private appService: AppService = Inject(AppService)) {}
+  dataSource!: MatTableDataSource<Person>;
+
+  mobileColumns: string[] = ['id', 'givenName', 'surName', 'photoURL'];
+  tabletColumns: string[] = ['id', 'givenName', 'surName', 'age', 'photoURL'];
+  desktopColumns: string[] = [
+    'id',
+    'givenName',
+    'surName',
+    'age',
+    'email',
+    'address',
+    'photoURL',
+  ];
+  displayedColumns: string[] = this.desktopColumns;
+
+  constructor(
+    private appService: AppService = Inject(AppService),
+    private breakpointObserver: BreakpointObserver
+  ) {}
 
   ngOnInit(): void {
     this.appService.getAllUsers().subscribe((users) => {
       this.users = users;
+
+      this.dataSource = new MatTableDataSource<Person>(this.users);
+      this.dataSource.sort = this.sort;
     });
+
+    this.breakpointObserver
+      .observe([
+        Breakpoints.XSmall,
+        Breakpoints.Small,
+        Breakpoints.Medium,
+        Breakpoints.Large,
+        Breakpoints.XLarge,
+      ])
+      .subscribe((result) => {
+        if (result.breakpoints[Breakpoints.XSmall]) {
+          this.displayedColumns = this.mobileColumns;
+        } else if (result.breakpoints[Breakpoints.Small]) {
+          this.displayedColumns = this.tabletColumns;
+        } else if (result.breakpoints[Breakpoints.Medium]) {
+          this.displayedColumns = this.tabletColumns;
+        } else if (result.breakpoints[Breakpoints.Large]) {
+          this.displayedColumns = this.desktopColumns;
+        } else if (result.breakpoints[Breakpoints.XLarge]) {
+          this.displayedColumns = this.desktopColumns;
+        }
+      });
   }
 }
